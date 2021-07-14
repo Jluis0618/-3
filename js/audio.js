77f3b734c7c3275f16e13498195706a847217dd7
Jluis0618 <85001208+Jluis0618@users.noreply.github.com>
2021-07-14 18:40:36 -0400

    Add files via upload

---
+++ js/audio.js
@@ -0,0 +1,119 @@
+const audio = document.getElementById("audio");
+const playPause = document.getElementById("play");
+
+playPause.addEventListener("click", () => {
+  if (audio.paused || audio.ended) {
+    playPause.querySelector(".pause-btn").classList.toggle("hide");
+    playPause.querySelector(".play-btn").classList.toggle("hide");
+    audio.play();
+  } else {
+    audio.pause();
+    playPause.querySelector(".pause-btn").classList.toggle("hide");
+    playPause.querySelector(".play-btn").classList.toggle("hide");
+  }
+});
+
+const audio2 = document.getElementById("audio2");
+const playPause2 = document.getElementById("play2");
+
+playPause2.addEventListener("click", () => {
+  if (audio2.paused || audio2.ended) {
+    playPause2.querySelector(".pause-btn").classList.toggle("hide");
+    playPause2.querySelector(".play-btn").classList.toggle("hide");
+    audio2.play();
+  } else {
+    audio2.pause();
+    playPause2.querySelector(".pause-btn").classList.toggle("hide");
+    playPause2.querySelector(".play-btn").classList.toggle("hide");
+  }
+});
+
+const audio3 = document.getElementById("audio3");
+const playPause3 = document.getElementById("play3");
+
+playPause3.addEventListener("click", () => {
+  if (audio3.paused || audio3.ended) {
+    playPause3.querySelector(".pause-btn").classList.toggle("hide");
+    playPause3.querySelector(".play-btn").classList.toggle("hide");
+    audio3.play();
+  } else {
+    audio3.pause();
+    playPause3.querySelector(".pause-btn").classList.toggle("hide");
+    playPause3.querySelector(".play-btn").classList.toggle("hide");
+  }
+});
+
+const audio4 = document.getElementById("audio4");
+const playPause4 = document.getElementById("play4");
+
+playPause4.addEventListener("click", () => {
+  if (audio4.paused || audio4.ended) {
+    playPause4.querySelector(".pause-btn").classList.toggle("hide");
+    playPause4.querySelector(".play-btn").classList.toggle("hide");
+    audio4.play();
+  } else {
+    audio4.pause();
+    playPause4.querySelector(".pause-btn").classList.toggle("hide");
+    playPause4.querySelector(".play-btn").classList.toggle("hide");
+  }
+});
+
+const audio5 = document.getElementById("audio5");
+const playPause5 = document.getElementById("play5");
+
+playPause5.addEventListener("click", () => {
+  if (audio5.paused || audio5.ended) {
+    playPause5.querySelector(".pause-btn").classList.toggle("hide");
+    playPause5.querySelector(".play-btn").classList.toggle("hide");
+    audio5.play();
+  } else {
+    audio5.pause();
+    playPause5.querySelector(".pause-btn").classList.toggle("hide");
+    playPause5.querySelector(".play-btn").classList.toggle("hide");
+  }
+});
+
+const audio6 = document.getElementById("audio6");
+const playPause6 = document.getElementById("play6");
+
+playPause6.addEventListener("click", () => {
+  if (audio6.paused || audio6.ended) {
+    playPause6.querySelector(".pause-btn").classList.toggle("hide");
+    playPause6.querySelector(".play-btn").classList.toggle("hide");
+    audio6.play();
+  } else {
+    audio6.pause();
+    playPause6.querySelector(".pause-btn").classList.toggle("hide");
+    playPause6.querySelector(".play-btn").classList.toggle("hide");
+  }
+});
+
+const audio7 = document.getElementById("audio7");
+const playPause7 = document.getElementById("play7");
+
+playPause7.addEventListener("click", () => {
+  if (audio7.paused || audio7.ended) {
+    playPause7.querySelector(".pause-btn").classList.toggle("hide");
+    playPause7.querySelector(".play-btn").classList.toggle("hide");
+    audio7.play();
+  } else {
+    audio7.pause();
+    playPause7.querySelector(".pause-btn").classList.toggle("hide");
+    playPause7.querySelector(".play-btn").classList.toggle("hide");
+  }
+});
+
+const audio8 = document.getElementById("audio8");
+const playPause8 = document.getElementById("play8");
+
+playPause8.addEventListener("click", () => {
+  if (audio8.paused || audio8.ended) {
+    playPause8.querySelector(".pause-btn").classList.toggle("hide");
+    playPause8.querySelector(".play-btn").classList.toggle("hide");
+    audio8.play();
+  } else {
+    audio8.pause();
+    playPause8.querySelector(".pause-btn").classList.toggle("hide");
+    playPause8.querySelector(".play-btn").classList.toggle("hide");
+  }
+});
---
+++ js/index.js
@@ -0,0 +1,99 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.nav', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+ 
+  sr.reveal('.wrapper', {
+     duration: 2000,
+     origin: 'bottom',
+     distance: '-100px'
+   });
+ 
+   sr.reveal('.fondo', {
+     duration: 2000,
+     origin: 'bottom',
+     distance: '-100px'
+   });
+ 
+   sr.reveal('.mensaje', {
+     duration: 2000,
+     origin: 'right',
+     distance: '-21px'
+   });
+ 
+   sr.reveal('.img-content', {
+     duration: 2000,
+     origin: 'left',
+     distance: '-21px'
+   });
+
+   sr.reveal('.title-regalos', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-40px'
+  });
+
+  sr.reveal('.regalos', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-40px'
+  });
+
+  sr.reveal('.title-gallery', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-40px'
+  });
+
+  sr.reveal('.img-gallery', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-21px'
+  });
+
+  sr.reveal('.title-versiculos', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-40px'
+  });
+  
+  sr.reveal('.item-versiculos', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-21px'
+  });
+
+  sr.reveal('.largo', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-40px'
+  });
+ 
+  sr.reveal('.title-peliculas', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-40px'
+  });
+  
+  sr.reveal('.item-peliculas', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-40px'
+  });
+
+  sr.reveal('.title-musica', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-40px'
+  });
+  
+  sr.reveal('.item-musica', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-40px'
+  });
+
+  
